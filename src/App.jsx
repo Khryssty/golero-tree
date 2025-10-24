8046c7fd1cbd10655d8a1030a74d7add6e14a106
Christy Grothues <122482422+Khryssty@users.noreply.github.com>
2025-10-24 10:52:12 -0400

    Added components and styles folder with header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Header from './components/header.jsx';
 import './App.css'
 
 function App() {
@@ -8,7 +9,9 @@ function App() {
 
   return (
     <>
+      <Header />
       <div>
+        
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ src/components/header.jsx
@@ -0,0 +1,17 @@
+import '../styles/header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">Golero Family Tree</div>
+      {/* <nav className="nav">
+        /Home</a>
+        <treeTree</a>
+        /aboutAbout</a>
+        <p>Welcome to the Golero Family Tree Application</p>
+      </nav> */}
+    </header>
+  );
+};
+
+export default Header;
