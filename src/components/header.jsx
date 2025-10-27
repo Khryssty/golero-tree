dbd4ecabf0381aaab20a7eaaf929f1d92207259c
Christy Grothues <122482422+Khryssty@users.noreply.github.com>
2025-10-27 08:48:33 -0400

    Added logos, data template.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,13 +3,10 @@ import '../styles/header.css';
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo">Golero Family Tree</div>
-      {/* <nav className="nav">
-        /Home</a>
-        <treeTree</a>
-        /aboutAbout</a>
-        <p>Welcome to the Golero Family Tree Application</p>
-      </nav> */}
+      <div className="logo">
+        <img src="/g-logo.png" alt="Tree Logo" className="logo-image" />
+        olero Family Tree
+      </div>
     </header>
   );
 };
